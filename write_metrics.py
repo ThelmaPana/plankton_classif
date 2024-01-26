85eb8de2587102bfe3c9d5bbe8aabb087e8a08bf
Thelma Panaiotis <thelma@crans.org>
2024-01-26 14:17:58 +0000

    Definitive set of taxonomy match

--- write_metrics.py
+++ write_metrics.py
@@ -34,18 +34,20 @@ df_all = pd.DataFrame()
 
 # List datasets and groups for each dataset
 datasets = ['flowcam', 'ifcb', 'isiis', 'uvp6', 'zoocam', 'zooscan']
-# Sheet is "Taxonomy match"
-sheet_id = '11_I-CZEVQTQqMwLuAI0N6m1RX8nWKE9ml8AgPtaCTcc'
+## Sheet is "Taxonomy match"
+#sheet_id = '11_I-CZEVQTQqMwLuAI0N6m1RX8nWKE9ml8AgPtaCTcc'
 
 
 for dataset in datasets:
     
     # Generate sheet url
-    sheet_name = dataset
-    url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
-    
+    #sheet_name = dataset
+    #url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
     # Reed sheet
-    df = pd.read_csv(url, usecols=['taxon', 'level2', 'plankton'])
+    #df = pd.read_csv(url, usecols=['taxon', 'level2', 'plankton'])
+    
+    # Read taxonomy match files
+    df = pd.read_csv(f'taxonomy_match/Taxonomy match - {dataset}.csv')
     
     # Add name of dataset
     df['dataset'] = dataset
