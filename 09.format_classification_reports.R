c36b32654ad70f7911198293bea1e9ea205c1cf8
Thelma Panaiotis <thelma@crans.org>
2025-10-31 09:58:24 +0000

    Update formatted CR for reviews

--- 09.format_classification_reports.R
+++ 09.format_classification_reports.R
@@ -10,6 +10,10 @@ library(gt)
 library(officer)
 library(openxlsx)
 
+# Directory where to save formatted reports
+reports_dir <- "reports/"
+dir.create(reports_dir, showWarnings = FALSE)
+
 
 ## List detailed classification reports ----
 #--------------------------------------------------------------------------#
@@ -22,7 +26,8 @@ tables <- tribble(
   "ifcb",    "IFCB",    "S3",
   "isiis",   "ISIIS",   "S4",
   "uvp6",    "UVP6",    "S5",
-  "zoocam",  "ZooCAM",  "S6"
+  "zoocam",  "ZooCAM",  "S6",
+  "zooscan", "Zooscan", "S7"
 )
 
 for (file in files) {
@@ -32,11 +37,12 @@ for (file in files) {
   # Read file
   df <- read_csv(file, show_col_types = FALSE) %>%
     # Select only F1 columns
-    select(taxon, grouped, plankton, contains("f1")) %>%
+    select(taxon, grouped, `n-native_rf`, plankton, contains("f1")) %>%
     # Rename columns with nice formatting
     rename(
       Taxon = taxon,
       Grouped = grouped,
+      `N test` = `n-native_rf`,
       `Nat + RF` = `f1-native_rf`,
       `Mob + MLP600` = `f1-mobilenet_pca50_rf`,
       `Eff S + MLP600` = `f1-mobilenet600`,
@@ -48,6 +54,7 @@ for (file in files) {
   if (dataset == "zooscan"){
     
     cr <- df %>%
+      mutate(plankton = str_c(plankton, " classes")) |> 
       gt(rowname_col = "Taxon", groupname_col = "plankton") %>%
       tab_stubhead(label = "Class") %>%
       tab_header(title = paste0("Classification report for detailed classes in the ", dataset, " dataset.")) %>%
@@ -82,14 +89,14 @@ for (file in files) {
       # No horizontal line in the table
       tab_options(table_body.hlines.width = 0) %>%
       # Cell colours
-      data_color(
-        columns = contains("+"),
-        method = "numeric",
-        palette = palette <- colorRampPalette(colors=c("#FFFFFF", "#A6D8DB"))(500),
-        domain = c(0, 1),
-        reverse = FALSE,
-        autocolor_text = FALSE,
-      ) %>%
+      #data_color(
+      #  columns = contains("+"),
+      #  method = "numeric",
+      #  palette = palette <- colorRampPalette(colors=c("#FFFFFF", "#A6D8DB"))(500),
+      #  domain = c(0, 1),
+      #  reverse = FALSE,
+      #  autocolor_text = FALSE,
+      #) %>%
       # Equal width for number columns
       cols_width(contains("+") ~ px(80)) %>% 
       # Change font
@@ -100,79 +107,82 @@ for (file in files) {
         )
       )
     
-    gtsave(cr, paste0("reports/", dataset, ".tex"))
-    gtsave(cr, paste0("reports/", dataset, ".docx"))
+    gtsave(cr, paste0(reports_dir, dataset, ".tex"))
+    gtsave(cr, paste0(reports_dir, dataset, ".docx"))
     
-  } else {# For others, save to Gsheet
-    
-    # Separate plankton from non plankton lines and convert values to percent
-    df_plankton <- df %>% filter(plankton == "Plankton") %>% select(-plankton) %>% mutate_if(is.numeric, ~(.x*100))
-    df_nonplankton <- df %>% filter(plankton != "Plankton") %>% select(-plankton) %>% mutate_if(is.numeric, ~(.x*100))
-    
-    # Prepare average summary rows
-    summ_plankton <- bind_cols(
-      tribble(
-        ~Taxon, ~Grouped,
-        "average", "",
-      ),
-      df_plankton %>% summarise_if(is.numeric, mean)
-    )
-    summ_nonplankton <- bind_cols(
-      tribble(
-        ~Taxon, ~Grouped,
-        "average", "",
-      ),
-      df_nonplankton %>% summarise_if(is.numeric, mean)
-    )
-    
-    # Add these rows to dataframes
-    df_plankton <- bind_rows(df_plankton, summ_plankton)
-    df_nonplankton <- bind_rows(df_nonplankton, summ_nonplankton)
-    
-    # Round numbers to 1 decimal
-    df_plankton <- df_plankton %>% mutate_if(is.numeric, ~round(.x, digits = 1))
-    df_nonplankton <- df_nonplankton %>% mutate_if(is.numeric, ~round(.x, digits = 1))
-    
-    # Prepare table caption
-    dataset_name <- tables %>% filter(dataset == !!dataset) %>% pull(dataset_name)
-    table_number <- tables %>% filter(dataset == !!dataset) %>% pull(table_number)
-    table_caption <- paste0("Table ", table_number, ": Classification report for detailed classes in the ", dataset_name, " dataset. Reported values are F1-scores. The models are described in Figure 1.")
-    
-    ## Write to excel
-    # Create workbook if it does not exist 
-    if (!exists("wb")) {
-      wb <- createWorkbook()  
-    }
-    
-    # Create tab for dataset
-    addWorksheet(wb, sheetName = dataset, gridLines = FALSE)
-    
-    # Write caption
-    writeData(wb, sheet = dataset, x = table_caption)
-    
-    # Write column names
-    writeData(wb, sheet = dataset, x = df_plankton %>% slice_head(n = 0), startCol = 1, startRow = 2)
-    
-    # Write "plankton" line
-    writeData(wb, sheet = dataset, x = "Plankton", startCol = 1, startRow = 3)
-    
-    # Write plankton report  with conditional formatting
-    writeData(wb, sheet = dataset, x = df_plankton, startCol = 1, startRow = 4, colNames = FALSE)
-    conditionalFormatting(
-      wb, sheet = dataset, type = "colourScale", cols = 3:6, rows = 4:(3 + nrow(df_plankton)),
-      style = c("white", "#A6D8DB"), rule = c(0, 100)
-    )
-    
-    # Write "non plankton" line
-    writeData(wb, sheet = dataset, x = "Non plankton", startCol = 1, startRow = nrow(df_plankton) + 4)
-    
-    # Write non plankton report with conditional formatting
-    writeData(wb, sheet = dataset, x = df_nonplankton, startCol = 1, startRow = nrow(df_plankton) + 5, colNames = FALSE)
-    conditionalFormatting(
-      wb, sheet = dataset, type = "colourScale", cols = 3:6, rows = (nrow(df_plankton) + 5):(nrow(df_plankton) + nrow(df_nonplankton) + 4),
-      style = c("white", "#A6D8DB"), rule = c(0, 100)
-    )
+  } 
+  
+  ## And save all to Gsheet
+  # Separate plankton from non plankton lines and convert values to percent
+  df_plankton <- df %>% filter(plankton == "Plankton") %>% select(-plankton) %>% mutate(across(`Nat + RF`:`Mob + PCA + RF`, ~(.x*100)))
+  df_nonplankton <- df %>% filter(plankton != "Plankton") %>% select(-plankton) %>% mutate(across(`Nat + RF`:`Mob + PCA + RF`, ~(.x*100)))
+  
+  # Prepare average summary rows
+  summ_plankton <- bind_cols(
+    tribble(
+      ~Taxon, ~Grouped, ~`N test`,
+      "average", "", "",
+    ) |> 
+      mutate(`N test` = as.numeric(`N test`)),
+    df_plankton |> summarise(across(`Nat + RF`:`Mob + PCA + RF`, mean))
+  )
+  summ_nonplankton <- bind_cols(
+    tribble(
+      ~Taxon, ~Grouped, ~`N test`,
+      "average", "", "",
+    ) |> 
+      mutate(`N test` = as.numeric(`N test`)),
+    df_nonplankton |> summarise(across(`Nat + RF`:`Mob + PCA + RF`, mean))
+  )
+  
+  # Add these rows to dataframes
+  df_plankton <- bind_rows(df_plankton, summ_plankton)
+  df_nonplankton <- bind_rows(df_nonplankton, summ_nonplankton)
+  
+  # Round numbers to 1 decimal
+  df_plankton <- df_plankton %>% mutate_if(is.numeric, ~round(.x, digits = 1))
+  df_nonplankton <- df_nonplankton %>% mutate_if(is.numeric, ~round(.x, digits = 1))
+  
+  # Prepare table caption
+  dataset_name <- tables %>% filter(dataset == !!dataset) %>% pull(dataset_name)
+  table_number <- tables %>% filter(dataset == !!dataset) %>% pull(table_number)
+  table_caption <- paste0("Table ", table_number, ": Classification report for detailed classes in the ", dataset_name, " dataset. Reported values are F1-scores. The models are described in Figure 1. N test indicates the number of objects in the test set for each class.")
+  
+  ## Write to excel
+  # Create workbook if it does not exist 
+  if (!exists("wb")) {
+    wb <- createWorkbook()  
   }
   
+  # Create tab for dataset
+  addWorksheet(wb, sheetName = dataset, gridLines = FALSE)
+  
+  # Write caption
+  writeData(wb, sheet = dataset, x = table_caption)
+  
+  # Write column names
+  writeData(wb, sheet = dataset, x = df_plankton %>% slice_head(n = 0), startCol = 1, startRow = 2)
+  
+  # Write "plankton" line
+  writeData(wb, sheet = dataset, x = "Plankton classes", startCol = 1, startRow = 3)
+  
+  # Write plankton report  with conditional formatting
+  writeData(wb, sheet = dataset, x = df_plankton, startCol = 1, startRow = 4, colNames = FALSE)
+  conditionalFormatting(
+    wb, sheet = dataset, type = "colourScale", cols = 4:7, rows = 4:(3 + nrow(df_plankton)),
+    style = c("white", "#ccebc5", "#4eb3d3"), rule = c(0, 50, 100)
+  )
+  
+  # Write "non plankton" line
+  writeData(wb, sheet = dataset, x = "Non plankton classses", startCol = 1, startRow = nrow(df_plankton) + 4)
+  
+  # Write non plankton report with conditional formatting
+  writeData(wb, sheet = dataset, x = df_nonplankton, startCol = 1, startRow = nrow(df_plankton) + 5, colNames = FALSE)
+  conditionalFormatting(
+    wb, sheet = dataset, type = "colourScale", cols = 4:7, rows = (nrow(df_plankton) + 5):(nrow(df_plankton) + nrow(df_nonplankton) + 4),
+    style = c("white", "#ccebc5", "#4eb3d3"), rule = c(0, 50, 100)
+  )
+}
+
 # Save the workbook
-saveWorkbook(wb, "perf/classif_reports_supp.xlsx", overwrite = TRUE)
+saveWorkbook(wb, "perf/classif_reports_supp_2.xlsx", overwrite = TRUE)
